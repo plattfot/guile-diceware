46564cd8f555b807a45f2a579c02b6a484f9476f
Fredrik Salomonsson <plattfot@posteo.net>
2020-09-18 12:22:09 -0700

    Init project

---
+++ hall.scm
@@ -0,0 +1,26 @@
+(hall-description
+  (name "diceware")
+  (prefix "guile")
+  (version "0.1")
+  (author "Fredrik Salomonsson")
+  (copyright (2020))
+  (synopsis "")
+  (description "")
+  (home-page "")
+  (license gpl3+)
+  (dependencies `())
+  (files (libraries
+           ((scheme-file "diceware")
+            (directory "diceware" ())))
+         (tests ((directory "tests" ())))
+         (programs ((directory "scripts" ())))
+         (documentation
+           ((org-file "README")
+            (symlink "README" "README.org")
+            (text-file "HACKING")
+            (text-file "COPYING")
+            (directory "doc" ((texi-file "diceware")))))
+         (infrastructure
+           ((scheme-file "guix")
+            (text-file ".gitignore")
+            (scheme-file "hall")))))
